024afba5e8bbe102f1b9929143a0af08ce2c0389
zourui <zourui@mrray.cn>
2018-03-21 18:25:45 +0800

    修改创建脱敏任务页面

--- src/modules/sensitive/createSensitive.vue
+++ src/modules/sensitive/createSensitive.vue
@@ -1,8 +1,43 @@
 <template>
     <div class="createSensitive">
-        <ContentHeader/>
-        <DataList></DataList>
-        <ContentFooter/>
+        <!--<ContentHeader/>-->
+        <!--<DataList></DataList>-->
+        <!--<ContentFooter/>-->
+        <div class="sensitiveEg">
+            <el-table
+                :data="tableData"
+                stripe
+                style="width: 100%">
+                <el-table-column prop="sensitiveData" label="敏感数据"></el-table-column>
+                <el-table-column prop="dataType" label="数据属性"></el-table-column>
+                <el-table-column prop="sensitiveType" label="脱敏方法"></el-table-column>
+                <el-table-column prop="sensitiveEg" label="脱敏实例"></el-table-column>
+            </el-table>
+        </div>
+        <div class="sensitiveType">
+            <el-table
+                :data="tableData"
+                stripe
+                :show-header="false"
+                style="width: 100%">
+                <el-table-column prop="sensitiveData" label="敏感数据"></el-table-column>
+                <el-table-column>
+                    <template slot-scope="scope">
+                        <el-radio v-model="scope.row.sensitiveType" label="掩码(*)">掩码(*)</el-radio>
+                    </template>
+                </el-table-column>
+                <el-table-column>
+                    <template slot-scope="scope">
+                        <el-radio v-model="scope.row.sensitiveType" label="掩码(×)">掩码(×)</el-radio>
+                    </template>
+                </el-table-column>
+                <el-table-column>
+                    <template slot-scope="scope">
+                        <el-radio v-model="scope.row.sensitiveType" label="不脱敏">不脱敏</el-radio>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
@@ -14,8 +49,37 @@
 
     export default {
         name: "create-sensitive",
-        components: {DataType,DataList,ContentHeader,ContentFooter}
+        components: {DataType,DataList,ContentHeader,ContentFooter},
+        data() {
+            return{
+                tableData: [
+                    {
+                        sensitiveData: 'teache',
+                        dataType: 'name',
+                        sensitiveType: '不脱敏',
+                        sensitiveEg: '张三丰 -> 张三丰'
+                    },
+                    {
+                        sensitiveData: 'teache',
+                        dataType: 'name',
+                        sensitiveType: '不脱敏',
+                        sensitiveEg: '张三丰 -> 张三丰'
+                    },
+                    {
+                        sensitiveData: 'teache',
+                        dataType: 'name',
+                        sensitiveType: '不脱敏',
+                        sensitiveEg: '张三丰 -> 张三丰'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .createSensitive{
+        background-color: #fff;
+        padding: 20px;
+    }
+</style>
